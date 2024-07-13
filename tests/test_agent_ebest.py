d7c4eeeaf87cfe5e0f38e3536b380e0c2a6e4aa1
01010-3 <gdhee7597@gmail.com>
2024-07-13 20:31:14 +0900

    mongoDB, API module for trading

--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -28,3 +28,36 @@ class TestEBest(unittest.TestCase):
         assert error_result is None
         print("result:", len(all_result), len(kosdaq_result), len(kospi_result))
 
+    def test_get_account_info(self):
+        result = self.ebest.get_account_info()
+        assert result is not None
+        print(result)
+
+    def test_get_account_stock_info(self):
+        result = self.ebest.get_account_stock_info()
+        assert result is not None
+        print(result)
+
+    def test_order_stock(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_stock("005930", "2", "50000", "2", "00")
+        assert result
+        print(result)
+
+    def test_order_cancel(self)L
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_cancel("29515", "A005930", "2")
+        assert result
+        print(result)
+
+    def test_order_check(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_check("29515")
+        assert result
+        print(result)
+
+    def test_get_current_call_price_by_code(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_current_call_price_by_code("005930")
+        assert result
+        print(result)
